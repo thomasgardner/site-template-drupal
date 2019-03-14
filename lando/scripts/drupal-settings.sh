dbeedf385e97c85ae8a0bcab14aeb021db9d8684
Kevin Wall <kevin@kwallcomany.com>
2019-03-14 11:39:38 -0700

    updated lando options to start and use template name... need this to perhaps by by branch or something? Next also bypass install screens

--- lando/scripts/drupal-settings.sh
+++ lando/scripts/drupal-settings.sh
@@ -7,3 +7,8 @@ if [ ! -f /app/docroot/sites/default/settings.local.php ]; then
 else
   echo "Drupal settings: settings.local.php exists"
 fi
+#TODO: Copy over public and private files into site
+rsync -av --progress /app/install/public /app/docroot/sites/default/files --exclude php
+mkdir /app/docroot/sites/default/files/private
+rsync -av --progress /app/install/private /app/docroot/sites/default/files/private/
+#TODO: Letsencrypt https?
