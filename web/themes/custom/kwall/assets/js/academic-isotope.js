8474d1e209d2bef5d375a7efccef2360591a698b
Ruslan Piskarov <ruslan.piskarev@gmail.com>
2019-10-25 09:27:12 +0300

    Code refactoring in the custom theme. Make the code more accurate and more closer to Drupal Best Practices. Guys, stop just copy-paste.

--- web/themes/custom/kwall/assets/js/academic-isotope.js
+++ web/themes/custom/kwall/assets/js/academic-isotope.js
@@ -3,57 +3,45 @@
   'use strict';
 
   /**
-   * Common tweaks for the theme.
+   * Academic Isotope.
    *
-   * @type {{attach: Drupal.behaviors.slickSliderInit.attach}}
+   * @type {{attach: Drupal.behaviors.academicIsotopeInit.attach}}
    */
   Drupal.behaviors.academicIsotopeInit = {
     attach: function (context, settings) {
+      $('.view-academics', context).once('academicIsotopeInit').each(function () {
+        var $grid = $(this).children('.view-content'),
+          $checkboxes = $(this).find('.filter-toggle'),
+          $reset = $(this).find('.isotope-reset');
 
-      $(document).ready(function () {
-
-        /*
-         * Academic Isotope Filter
-        */
-        // init Isotope
-        $('.view-academics', context).once('academicIsotopeInit').each(function () {
-
-          var $grid = $(this).children('.view-content'),
-            $checkboxes = $(this).find('.filter-toggle'),
-            $reset = $(this).find('.isotope-reset');
-
-          $grid.isotope({
-            itemSelector: '.grid-item',
-            layoutMode: 'fitRows'
-          });
-
-          // bind filter button click
-          $checkboxes.on('click', function () {
-            var filters = [];
-            $(this).toggleClass('active');
-            // get checked checkboxes values
-            $checkboxes.filter('.active').each(function () {
-              filters.push($(this).data('tid'));
-            });
-            filters = filters.join(', ');
-            $grid.isotope({filter: filters});
-          });
-
-          // bind filter rest
-          $reset.click(function () {
-            $checkboxes.each(function () {
-              $(this).removeClass('active');
-            });
-            $grid.isotope({filter: '*'});
-          });
-
+        $grid.isotope({
+          itemSelector: '.grid-item',
+          layoutMode: 'fitRows'
         });
 
+        // Bind filter button click.
+        $checkboxes.on('click', function () {
+          var filters = [];
 
-      }); // END $(document).ready
+          $(this).toggleClass('active');
+          // Get checked checkboxes values.
+          $checkboxes.filter('.active').each(function () {
+            filters.push($(this).data('tid'));
+          });
+          filters = filters.join(', ');
+          $grid.isotope({filter: filters});
+        });
 
+        // Bind filter rest.
+        $reset.click(function () {
+          $checkboxes.each(function () {
+            $(this).removeClass('active');
+          });
+          $grid.isotope({filter: '*'});
+        });
+
+      });
     }
   };
-
 
 })(jQuery, Drupal, drupalSettings);
--- web/themes/custom/kwall/assets/js/scripts.js
+++ web/themes/custom/kwall/assets/js/scripts.js
@@ -5,15 +5,11 @@
   /**
    * Common tweaks for the theme.
    *
-   * @type {{attach: Drupal.behaviors.flexSliderInit.attach}}
+   * @type {{attach: Drupal.behaviors.commonTweaks.attach}}
    */
-  Drupal.behaviors.commnTweaks = {
+  Drupal.behaviors.commonTweaks = {
     attach: function (context, settings) {
-
-
-      /**
-       * background image paralax effect
-       **/
+      // Background image Parallax effect.
       $(window).on('load', function () {
         $('.full-width-img-section', context).once('commnTweaks').each(function () {
           var $img_section = $(this);
@@ -26,16 +22,14 @@
             var controller = new ScrollMagic.Controller(),
               $scroll_duration = $(window).height() + $img_section.outerHeight(),
               paragraph_id = '#' + $img_section.attr('id'),
-              scroll_speed = ($img_section.data('parallax-speed') == '') ? '5' : $img_section.data('parallax-speed');
+              scroll_speed = ($img_section.data('parallax-speed') === '') ? '5' : $img_section.data('parallax-speed');
 
-            // build scene
+            // Build scene.
             var scene = new ScrollMagic.Scene({
               triggerElement: paragraph_id,
               duration: $scroll_duration,
               triggerHook: 'onEnter'
-            })
-              .addTo(controller)
-              // .addIndicators() // remove for production
+            }).addTo(controller)
               .on("progress", function (e) {
                 $img_section.css('background-position', 'center ' + (e.progress.toFixed(2) * (100 / scroll_speed)) + '%');
               });
@@ -43,185 +37,145 @@
         });
       });
 
-
-      $(document).ready(function () {
-
-        /*
-         * Hero Banner
-         */
-        $('.hero-banner-img-section', context).once('setHeroBannerImg').each(function () {
-          var bg_img_url = $(this).data('bg-img');
-          $(this).css({
-            'background-image': 'url(' + bg_img_url + ')',
-            'background-size': 'cover',
-            'background-position': 'center top'
-          })
-        });
-
-        /*
-         * Featured Article Background Image
-         */
-        $('.view-id-article_view.view-display-id-attachment_1 .featured-post-wrap', context).once('setFeaturedArticleImage').each(function () {
-          var bg_img = $(this).data('featured-article');
-          $(this).parents('.featured-news-grid.view-display-id-block_1 .attachment').css({
-            'background-image': 'url(' + bg_img + ')',
-            'background-size': 'cover',
-            'background-position': 'center center'
-          });
-        });
-
-        /*
-         * Featured Article Background Image
-         */
-        $('.view-id-event_view.view-display-id-attachment_1 .featured-event-wrap', context).once('setFeaturedArticleImage').each(function () {
-          var bg_img = $(this).data('featured-event');
-          $(this).parents('.featured-events-grid.view-display-id-block_3 .attachment').css({
-            'background-image': 'url(' + bg_img + ')',
-            'background-size': 'cover',
-            'background-position': 'center center'
-          });
-        });
-
+      // Hero Banner.
+      $('.hero-banner-img-section', context).once('setHeroBannerImg').each(function () {
+        $(this).css({
+          'background-image': 'url(' + $(this).data('bg-img') + ')',
+          'background-size': 'cover',
+          'background-position': 'center top'
+        })
       });
 
-      $('.paragraph--type--accordion', context).once('commnTweaks').each(function () {
+      // Featured Article Background Image.
+      $('.view-id-article_view.view-display-id-attachment_1 .featured-post-wrap', context).once('setFeaturedArticleImage').each(function () {
+        $(this).parents('.featured-news-grid.view-display-id-block_1 .attachment').css({
+          'background-image': 'url(' + $(this).data('featured-article') + ')',
+          'background-size': 'cover',
+          'background-position': 'center center'
+        });
+      });
+
+      // Featured Article Background Image.
+      $('.view-id-event_view.view-display-id-attachment_1 .featured-event-wrap', context).once('setFeaturedArticleImage').each(function () {
+        var bg_img = $(this).data('featured-event');
+        $(this).parents('.featured-events-grid.view-display-id-block_3 .attachment').css({
+          'background-image': 'url(' + $(this).data('featured-event') + ')',
+          'background-size': 'cover',
+          'background-position': 'center center'
+        });
+      });
+
+      $('.paragraph--type--accordion', context).once('accordionInit').each(function () {
         var $toggle_all = $(this).find('.toggle-all');
 
         if ($toggle_all.length) {
           var $accordion_parent = $(this),
-            $accordion_parent_id = $(this).attr('id'),
             $accordion_item = $(this).find('.collapse');
 
           $toggle_all.on('click', function () {
-
             if (!$accordion_parent.hasClass('show-all')) {
-
               $accordion_item.each(function () {
                 $(this).collapse('show');
               });
               $accordion_parent.addClass('show-all');
-
             }
             else {
-
               $accordion_item.each(function () {
                 $(this).collapse('hide');
               });
               $accordion_parent.removeClass('show-all');
-
             }
-
           });
         }
       });
-
-
     }
   };
 
-
   /**
-   * Common tweaks for the theme.
+   * Sidebar Accordion.
    *
    * @type {{attach: Drupal.behaviors.sidebarAccordionInit.attach}}
    */
   Drupal.behaviors.sidebarAccordionInit = {
     attach: function (context, settings) {
-
-      /*
-       * Sidebar Accordion Toggle
-       */
+      // Sidebar Accordion Toggle.
       var $sidebar_menu = $('.sidebar-menu-block', context);
-      // Sidebar navigation toggle.
-      $(document).ready(function () {
-        $sidebar_menu.once('sidebarAccordionInit').each(function () {
-          // Open active submenu.
-          $(this).find('.expanded.dropdown.active').addClass('open');
-          var $toggler = $(this).find('.expanded.dropdown-item .fa');
 
-          $toggler.on('click', function () {
+      // Sidebar navigation toggle.
+      $sidebar_menu.once('sidebarAccordionInit1').each(function () {
+        // Open active submenu.
+        $(this).find('.expanded.dropdown.active').addClass('open');
+        var $toggler = $(this).find('.expanded.dropdown-item .fa');
+
+        $toggler.on('click', function () {
+          $(this).toggleClass('fa-angle-down fa-angle-up');
+          $(this).parent('li').toggleClass('toggled');
+          $(this).siblings($(this).attr('data-toggle')).slideToggle();
+        });
+        // Toggle on 'Enter' keypress.
+        $toggler.keypress(function (e) {
+          if (e.keyCode === 13) {
             $(this).toggleClass('fa-angle-down fa-angle-up');
             $(this).parent('li').toggleClass('toggled');
             $(this).siblings($(this).attr('data-toggle')).slideToggle();
-          });
-          //  Toggle on 'Enter' keypress
-          $toggler.keypress(function (e) {
-            if (e.keyCode == 13) {
-              $(this).toggleClass('fa-angle-down fa-angle-up');
-              $(this).parent('li').toggleClass('toggled');
-              $(this).siblings($(this).attr('data-toggle')).slideToggle();
-            }
-          });
+          }
         });
+      });
 
-        $sidebar_menu.find('.is-active').once('sidebarAccordionInit').each(function () {
-          var $parents = $(this).parents('.dropdown-menu-list'),
-            $is_active = $(this).parent('.dropdown-item');
-          $parents.siblings('.fa-angle-down').toggleClass('fa-angle-down fa-angle-up');
-          $parents.slideDown();
-          $is_active.children('.fa-angle-down').toggleClass('fa-angle-down fa-angle-up');
-          $is_active.addClass('toggled').children('.dropdown-menu-list').slideDown();
-        });
+      $sidebar_menu.find('.is-active').once('sidebarAccordionInit2').each(function () {
+        var $parents = $(this).parents('.dropdown-menu-list'),
+          $is_active = $(this).parent('.dropdown-item');
 
-
-      });// END $(document)ready
-
+        $parents.siblings('.fa-angle-down').toggleClass('fa-angle-down fa-angle-up');
+        $parents.slideDown();
+        $is_active.children('.fa-angle-down').toggleClass('fa-angle-down fa-angle-up');
+        $is_active.addClass('toggled').children('.dropdown-menu-list').slideDown();
+      });
     }
   };
 
-
   /**
-   * Common tweaks for the theme.
+   * Calender Rest API.
    *
-   * @type {{attach: Drupal.behaviors.sidebarAccordionInit.attach}}
+   * @type {{attach: Drupal.behaviors.calenderRestAPI.attach}}
    */
   Drupal.behaviors.calenderRestAPI = {
     attach: function (context, settings) {
+      $('.calendar-month', context).once('calenderRestAPI').each(function () {
+        $(this).find('.event-content').on('click', function () {
+          var date = $(this).data('date-trigger'),
+            parent = $(this).parent('.contents');
 
-      /*
-       * Sidebar Accordion Toggle
-       */
-      $(document).ready(function () {
+          if (!parent.hasClass('import')) {
+            $.getJSON("/rest/events/day/" + date, function (data) {
+              var items = [],
+                count = data.length - 1;
 
-        $('.calendar-month', context).once('calenderRestAPI').each(function () {
-          $(this).find('.event-content').on('click', function () {
-            var date = $(this).data('date-trigger'),
-              parent = $(this).parent('.contents');
-
-            if (!parent.hasClass('import')) {
-              $.getJSON("/rest/events/day/" + date, function (data) {
-                var items = [],
-                  count = data.length - 1;
-
-                $.each(data, function (key, val) {
-                  if (key == 0) {
-                    items.push("<li><span class='fa fa-close'></span></li>");
-                  }
-                  items.push("<li id='event-" + date + '-' + key + "'>" + val.nothing + "</li>");
-                  if (key >= 2 && key == count) {
-                    items.push("<li><a href='/events/day/" + date + "'>View All <i class='fa fa-chevron-right'></i></a></li>");
-                  }
-                });
-
-                $("<ul/>", {
-                  "class": "popup list-unstyled",
-                  html: items.join("")
-                }).appendTo(parent);
-
+              $.each(data, function (key, val) {
+                if (key === 0) {
+                  items.push("<li><span class='fa fa-close'></span></li>");
+                }
+                // FIXME: Unresolved variable val.nothing.
+                items.push("<li id='event-" + date + '-' + key + "'>" + val.nothing + "</li>");
+                if (key >= 2 && key === count) {
+                  items.push("<li><a href='/events/day/" + date + "'>" + Drupal.t('View All') + " <i class='fa fa-chevron-right'></i></a></li>");
+                }
               });
-              parent.addClass('import open');
-            }
-            else {
-              parent.toggleClass('open close');
-            }
 
-          });
+              $("<ul/>", {
+                "class": "popup list-unstyled",
+                html: items.join("")
+              }).appendTo(parent);
+
+            });
+            parent.addClass('import open');
+          }
+          else {
+            parent.toggleClass('open close');
+          }
         });
-
       });
-
     }
   };
-
 
 })(jQuery, Drupal, drupalSettings);
