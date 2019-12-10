67afc730ee6a0e258ab1a8e1be2f1e690c143b9e
Ben Rathbone <ben.rathbone@kwallcompany.com>
2019-12-10 13:11:47 -0800

    Removed Shawn's e-mail from config and compressed backups.

--- backups/2019-05-31-930p.sql
+++
@@ -1,3 +0,0 @@
-The Drush launcher could not find a local Drush in your Drupal site.
-Please add Drush with Composer to your project.
-Run 'cd "/var/www/template/d8/docroot" && composer require drush/drush'
