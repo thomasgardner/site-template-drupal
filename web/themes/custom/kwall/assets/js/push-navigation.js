ff0a47d03cfc55999b3392e46900cc2185ab2b9a
Patrick Wall <patrick@kwallcompany.com>
2020-02-25 10:22:11 -0800

    adding keyboard navigation

--- web/themes/custom/kwall/assets/js/push-navigation.js
+++ web/themes/custom/kwall/assets/js/push-navigation.js
@@ -29,9 +29,76 @@
           $('body').removeClass('overflow-hide');
         }
       });
+      $(document).on('keypress',function(e){
+        if ( e.which == 13 && !$('.push-menu-toggle').is(e.target) && !$('.layout-push-navigation').is(e.target) && $('.layout-push-navigation').has(e.target).length === 0 && $('.layout-push-navigation').hasClass('open') ) {
+          $('.layout-push-navigation').removeClass('active');
+          $('body').removeClass('overflow-hide');
+        }
+      });
       */
 
       $.fn.mobileMenuToggle = function (theElement) {
+        $(this).on('keypress', theElement, function (e) {
+          if (e.which == 13) {
+            var menuName = $(this).closest('.fa').attr('title');
+
+            menuName = menuName.toLowerCase().replace(' ', '-');
+
+            if ($('.child-menu-container.' + menuName).length === 0) {
+              var $subMenu = $(this).parent('.menu__item').clone(true, true),
+                $parent_link = $subMenu.html().split('</a>')[0] + '</a>',
+                $sub_heading = $($parent_link).text(),
+                $sub_heading_attr = $($parent_link).attr('title'),
+                $children_links = $subMenu.html().replace($parent_link, ''),
+                $sub_head = '';
+
+              // Add description to slide in menu.
+              if ($sub_heading_attr === '' || typeof $sub_heading_attr === undefined) {
+                $sub_head = '<h5 class="sub-head">' + $sub_heading + ' Overview</h5>';
+              }
+              else {
+                $sub_head = '<h5 class="sub-head">' + $sub_heading_attr + '</h5>';
+              }
+
+              $(this).next('.dropdown-menu-list').remove();
+              $('.layout-push-navigation').append('<div class="child-menu-container ' + menuName + '">' +
+                '<h2>' + $parent_link + '</h2>' +
+                '<span class="prev-menu"><i class="fa fa-angle-left"></i> Back</span>' +
+                // Add description to slide in menu.
+                $sub_head +
+                '<ul class="parent-wrap">' + $children_links + '</ul>' +
+                '</div>');
+              setTimeout(function () {
+                $('.child-menu-container.' + menuName).addClass('active');
+                var focusLink = $('.child-menu-container.' + menuName + ' .dropdown-menu-list .menu-list-1 .menu__item:first a');
+                if (focusLink.length > 0) {
+                  focusLink.focus();
+                }
+                else {
+                  var focusLink = $('.child-menu-container.' + menuName + ' .dropdown-menu-list .menu-list-1 .menu__item:nth-child(2) a');
+                  focusLink.focus();
+                }
+              }, 500);
+              $('.child-menu-container.' + menuName + ' .prev-menu').on('keypress', function (e) {
+                if (e.which == 13) {
+                  e.preventDefault();
+                  if ($(this).parent('.child-menu-container').hasClass('active')) {
+                    $(this).parent('.child-menu-container').removeClass('active');
+                  }
+                }
+              });
+              $('.child-menu-container.' + menuName + ' .prev-menu').on('click touchstart', function (e) {
+                e.preventDefault();
+                if ($(this).parent('.child-menu-container').hasClass('active')) {
+                  $(this).parent('.child-menu-container').removeClass('active');
+                }
+              });
+            }
+            else {
+              $('.child-menu-container.' + menuName).addClass('active');
+            }
+          }
+        });
         $(this).on('click touchstart', theElement, function (e) {
           var menuName = $(this).closest('.fa').attr('title');
 
@@ -64,6 +131,14 @@
             setTimeout(function () {
               $('.child-menu-container.' + menuName).addClass('active');
             }, 500);
+            $('.child-menu-container.' + menuName + ' .prev-menu').on('keypress', function (e) {
+              if (e.which == 13) {
+                e.preventDefault();
+                if ($(this).parent('.child-menu-container').hasClass('active')) {
+                  $(this).parent('.child-menu-container').removeClass('active');
+                }
+              }
+            });
             $('.child-menu-container.' + menuName + ' .prev-menu').on('click touchstart', function (e) {
               e.preventDefault();
               if ($(this).parent('.child-menu-container').hasClass('active')) {
