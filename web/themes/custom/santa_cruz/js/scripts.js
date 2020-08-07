b0b6325bfe31626d0aff180456ee1514e7b1e40b
Ruslan Piskarov <ruslan.piskarev@gmail.com>
2020-08-07 17:03:28 +0300

    Feature Slideshow.

--- web/themes/custom/santa_cruz/js/scripts.js
+++ web/themes/custom/santa_cruz/js/scripts.js
@@ -3,15 +3,45 @@
  * Placeholder file for custom sub-theme behaviors.
  *
  */
-(function ($, Drupal) {
+(function ($, Drupal, drupalSettings) {
+
+  'use strict';
 
   /**
-   * Use this behavior as a template for custom Javascript.
+   * Use for short things.
+   *
+   * @type {{attach: Drupal.behaviors.common.attach}}
    */
-  Drupal.behaviors.exampleBehavior = {
+  Drupal.behaviors.common = {
     attach: function (context, settings) {
-      //alert("I'm alive!");
+
     }
   };
 
-})(jQuery, Drupal);
+  /**
+   * Featured Slideshow.
+   *
+   * @type {{attach: Drupal.behaviors.featureSlideshow.attach}}
+   */
+  Drupal.behaviors.featureSlideshow = {
+    attach: function (context, settings) {
+      var $context = $(context);
+
+      $context.find('.owl-carousel', context).owlCarousel({
+        autoHeight: true,
+        nav: true,
+        dots: false,
+        responsive: {
+          0: {
+            items: 1
+          },
+          900: {
+            items: 2
+          }
+        }
+      });
+    }
+  };
+
+
+})(jQuery, Drupal, drupalSettings);
